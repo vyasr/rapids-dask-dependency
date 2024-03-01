fd3f53eb95812b9cfee7d27671be21b004a90c64
rjzamora <rzamora217@gmail.com>
2024-02-29 20:24:34 -0800

    add test coverage as example

--- rapids_dask_dependency/patches/dask_patch.py
+++ rapids_dask_dependency/patches/dask_patch.py
@@ -13,9 +13,9 @@ class DaskPatch(BasePatch):
         # Add top-level attribute
         mod.test_attr = "hello world"
 
-        # Add attribute to dask.dataframe.io
-        ddio = importlib.import_module("dask.dataframe.io")
-        ddio.test_attr = "hello world"
+        # Add attribute to dask._compatibility
+        dc = importlib.import_module("dask._compatibility")
+        dc.test_attr = "hello world"
 
 
 dask_patcher.register(DaskPatch)
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from multiprocessing import Process
 
+import pytest
+
 
 def run_test_in_subprocess(func):
     @wraps(func)
@@ -17,6 +19,22 @@ def test_dask():
     import dask
 
     assert hasattr(dask, "test_attr")
+
+
+@run_test_in_subprocess
+def test_dask_dataframe_deprecation():
+    with pytest.warns(DeprecationWarning):
+        pass
+
+
+@run_test_in_subprocess
+def test_dask_compatibility():
+    import dask._compatibility as dc
+
+    assert hasattr(dc, "test_attr")
+
+    with pytest.warns(DeprecationWarning):
+        dc.entry_points()
 
 
 @run_test_in_subprocess
